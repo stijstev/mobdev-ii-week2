d15f4dfcc7b171920713d3d269d6e296d59c4050
stijstev <stijstev@student.arteveldehs.be>
2018-02-20 15:39:54 +0100

    Add JSON data

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
 const server = http.createServer(app);
 
@@ -12,25 +14,83 @@ if(nodeEnv !== 'production') {
 const hostName = '127.0.0.1';
 const port = '8080';
 
-
+/*================================================*/
+// Middleware
+/*================================================*/
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+/*================================================*/
+// Routes
+/*================================================*/
 app.get('/', (req, resp) => {
     resp.send('Hello world!')
 })
 
-app.use((req, resp, next) => {
-    const err = new Error('Not found!');
-    err.status = 400;
-    next(err);
+app.get('/hello', (req, resp) => {
+    resp.json({
+        "message": "Hello world!"
+    })
 })
 
-app.use((err, req, resp) => {
-    resp.locals.message = err.message;
-    resp.status(err.status || 500);
-    resp.send('Error');
+app.get('/students', (req, resp) => {
+    resp.json([
+        {
+            "name": "Billy",
+            "lastname": "Billyson",
+        },
+        {
+            "name": "Andy",
+            "lastname": "Anderson",
+        },
+    ])
 })
+
+app.get('/students/:studentId', (req, resp) => {
+    resp.json([
+        {
+            "name": "Billy",
+            "lastname": "Billyson",
+        },
+    ])
+})
+
+app.get('/posts', (req, resp) => {
+    resp.json([
+        {
+            "title": "Whut",
+            "desc": "Hello world",
+        },
+        {
+            "title": "Yeah",
+            "desc": "Lolwut",
+        },
+    ])
+})
+
+app.get('/posts/:postId', (req, resp) => {
+    resp.json([
+        {
+            "title": "Whut",
+            "desc": "Lolwut",
+        },
+    ])
+})
+
+// app.use((req, resp, next) => {
+//     const err = new Error('Not found!');
+//     err.status = 400;
+//     next(err);
+// })
+
+// app.use((err, req, resp) => {
+//     resp.locals.message = err.message;
+//     resp.status(err.status || 500);
+//     resp.send('Error');
+// })
 
 server.listen(port, hostName, () => {
     console.log(`Node server running at http://${hostName}:${port}/!`)
